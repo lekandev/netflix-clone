7115689247c1e6c843687ed08bc1ae91ed0011cb
Mohammed Kabir Hussaini <65419835+lekandev@users.noreply.github.com>
2021-02-09 00:44:15 +0100

    fetched the movie data using Axios

--- src/Row.js
+++ src/Row.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from './axios';
 
-function Row() {
+function Row({ title, fetchUrl }) {
+    const [movies, setMovies] = useState([]);
+
+    // A snippet of code which runs based on a specific condition/variable
+    useEffect(() => {
+       // if [], run once when the page loads and don't run again
+       async function fetchData() {
+           const request = await axios.get(fetchUrl);
+           // "https://api.themoviedb.org/3/discover/tv?api_key=${API_KEY}&with_genres=28"
+           console.log(request);
+           return request;
+       }
+       fetchData();
+    }, []);
+
     return (
         <div>
-            
+            <h2>{title}</h2>
+
+            {/* container -> posters */}
         </div>
     )
 }
