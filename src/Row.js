02bbab71b16a13033fab1e5ec1d283cc262c972c
Mohammed Kabir Hussaini <65419835+lekandev@users.noreply.github.com>
2021-02-23 01:40:31 +0100

    🚣‍♀️🤩 Rows with dynamic movie data from tmdb API

--- src/Row.js
+++ src/Row.js
@@ -1,28 +1,42 @@
-import React, { useState, useEffect } from 'react';
-import axios from './axios';
+import React, { useState, useEffect } from "react";
+import axios from "./axios";
+import "./Row.css";
 
-function Row({ title, fetchUrl }) {
-    const [movies, setMovies] = useState([]);
+const base_url = "http://image.tmdb.org/t/p/original/";
 
-    // A snippet of code which runs based on a specific condition/variable
-    useEffect(() => {
-       // if [], run once when the page loads and don't run again
-       async function fetchData() {
-           const request = await axios.get(fetchUrl);
-           // "https://api.themoviedb.org/3/discover/tv?api_key=${API_KEY}&with_genres=28"
-           console.log(request);
-           return request;
-       }
-       fetchData();
-    }, []);
+function Row({ title, fetchUrl, isLargeRow }) {
+  const [movies, setMovies] = useState([]);
 
-    return (
-        <div>
-            <h2>{title}</h2>
+  // A snippet of code which runs based on a specific condition/variable
+  useEffect(() => {
+    // if [], run once when the page loads and don't run again
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      // "https://api.themoviedb.org/3/discover/tv?api_key=${API_KEY}&with_genres=28"
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchData();
+  }, [fetchUrl]);
 
-            {/* container -> posters */}
-        </div>
-    )
+  return (
+    <div className="row">
+      <h2>{title}</h2>
+
+      <div className="row__posters">
+        {/* row__poster */}
+
+        {movies.map((movie) => (
+          <img
+            key={movie.id}
+            className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+            src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`}
+            alt={movie.name}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default Row
+export default Row;
